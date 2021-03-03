7691ddd3d5d6fc39d0b5134a04fa1e030006b1d2
toni-santos <a.oliveirasantos122@gmail.com>
2021-03-03 17:30:07 +0000

    added excs 2.1--2.2c

---
+++ cpp/f1_2_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float a,b,c,d,e,f; //values
+    float x,y; //incognitas
+
+    cout << "dizer abcdef" << endl;
+    cin >> a >> b >> c >> d >> e >> f;
+    x = (c*e-b*f) / (a*e-b*d);
+    y = (a*f-c*d) / (a*e-b*d);
+    if ((a*e-b*d)==0){
+        cout << "muridayo" << endl;
+    }else if (x==0 || y==0){
+        cout << "ikkan-sei no nai" << endl;
+    }else{
+        cout << x << " " << y << endl;
+    }
+
+    return 0;
+}
---
+++ cpp/f1_2_2b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int inpt[3];
+    int outpt[3]={0,0,0};
+    int i;
+
+    for (i=0; i<3; i++){
+        cout<<"number "<<i+1<<":\t";
+        cin>>inpt[i];
+    }
+    
+    if (inpt[0]>=inpt[1] && inpt[0]>=inpt[2] && inpt[1]>=inpt[2]){
+        outpt[0]=inpt[0];outpt[1]=inpt[1];outpt[2]=inpt[2];
+    }else if (inpt[0]>=inpt[1] && inpt[0]>=inpt[2] && inpt[2]>=inpt[1]){
+        outpt[0]=inpt[0];outpt[1]=inpt[2];outpt[2]=inpt[1];
+    }else if(inpt[1]>=inpt[0] && inpt[1]>=inpt[2] && inpt[0]>=inpt[2]){
+        outpt[0]=inpt[1];outpt[1]=inpt[0];outpt[2]=inpt[2];
+    }else if(inpt[1]>=inpt[0] && inpt[1]>=inpt[2] && inpt[2]>=inpt[0]){
+        outpt[0]=inpt[1];outpt[1]=inpt[2];outpt[2]=inpt[0];
+    }else if(inpt[1]>=inpt[0]){
+        outpt[0]=inpt[2];outpt[1]=inpt[1];outpt[2]=inpt[0];
+    }else{
+        outpt[0]=inpt[2];outpt[1]=inpt[0];outpt[2]=inpt[1];
+    }
+    
+    for (i=0; i<3; i++){
+        cout<<outpt[i]<<" ";
+    }
+
+    return 0;
+}
