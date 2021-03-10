bf6b4b1f108da53180be1ddbe1edeb37c308d26e
toni-santos <a.oliveirasantos122@gmail.com>
2021-03-10 19:18:09 +0000

    Create f1_2_6.cpp

---
+++ cpp/f1_2_6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    float n, q, j, amnt;
+
+    cout << "n,q,j" << endl;
+    cin >> n >> q >> j;
+
+    amnt = q*pow(1+(j/100),n);
+
+    cout << amnt << endl;
+
+    return 0;
+}
